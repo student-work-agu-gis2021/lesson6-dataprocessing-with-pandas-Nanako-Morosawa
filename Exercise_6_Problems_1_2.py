5cc2e90ce14374ac3e8b0a9470ada4e3f12aed90
NanakoMorosawa <>
2021-05-23 16:45:21 +0000

    Find the date of the most recent (last) observation.

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -90,6 +90,7 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
+#Find the date of the most recent (last) observation
 last_obs=data.iloc[day_count-1,4]
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
